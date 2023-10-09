701a6ec2bbeaff62da9ac5d8d17e1f737e70cf50
juliawatanabe <julia.awv@gmail.com>
2023-10-09 16:01:32 -0300

    changed layour and added some properties into the components script

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css']
 })
-export class BigCardComponent {
+
+export class BigCardComponent implements OnInit {
+  
+  @Input()
+  photoCover:string = "" 
+  // o photoCover é string porque passamos o link da img aqui e chamamos a propriedade no src da html
+  @Input()
+  cardTitle:string = ""
+  @Input()
+  cardDescription:string = ""
+  //o input fará com que todas as propriedades acima possam ser inputáveis em qualquer tag big-card que chamarmos
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
